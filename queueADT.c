5d8d1b74ceef8261283c06acbc16de2a3aec5180
Kevin Joseph Becker <kjb2503@cs.rit.edu>
2018-03-13 18:50:48 -0400

    Added and implemented the que_remove(1) function.

--- queueADT.c
+++ queueADT.c
@@ -41,7 +41,22 @@ QueueADT que_create( int (*cmp)(const void*a,const void*b) )
 
 void * que_remove( QueueADT queue )
 {
+    // ( last ) QNode -> QNode -> QNode -> QNode ( first )
+    // pulls in our first node
+    QNode *firstNode = queue->first;
 
+    // copies the first node's data into temp var
+    void* toReturn = firstNode->data;
+    // changes our first node to be the second member
+    queue->first = firstNode->next;
+    // subtracts one from the size
+    queue->size -= 1;
+
+    // destroys our first node (we're done with it)
+    free(firstNode);
+
+    // returns the value we just removed
+    return toReturn;
 }
 
 
