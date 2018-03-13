2acb126cbbabf2a37451520327879e5e9afb6f3d
Kevin Joseph Becker <kjb2503@cs.rit.edu>
2018-03-13 18:51:43 -0400

    Added and implemented the que_create function.

--- queueADT.c
+++ queueADT.c
@@ -35,7 +35,81 @@ typedef struct queue_s{
 
 QueueADT que_create( int (*cmp)(const void*a,const void*b) )
 {
+    // pointer to a new queue (set to NULL for now)
+    QueueADT queue = NULL;
+    // allocates enough space for our queue
+    queue = malloc(sizeof(queue_s));
+    // sets our first and last nodes to null
+    queue->first = NULL;
+    queue->last = NULL;
+    // current size is 0
+    queue->size = 0;
+    // sets our comparison function (which may be NULL) to the one passed in
+    queue->cmp = cmp;
 
+    // returns our new, empty queue
+    return queue;
+}
+
+
+void que_destroy( QueueADT queue )
+{
+    
+}
+
+
+void que_clear( QueueADT queue )
+{
+    
+}
+
+
+void que_insert( QueueADT queue, void * data )
+{
+    // creats a new node
+    QNode *newNode = NULL;
+    newNode = malloc(sizeof(QNode));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    // if we have an empty queue we can immediately just put the node in
+    // (no comparison needed)
+    if(queue->size == 0)
+    {
+        // sets both the first and last element to new node
+        queue->first = queue->last = newNode;
+        // we can immediately return here we are done
+        return;
+    }
+    // if we weren't given a comparison function, FIFO style
+    else if(queue->cmp == NULL)
+    {
+        // last node's next member is set to our new node
+        queue->last->next = newNode;
+        // so is the last element of queue set to our new node
+        queue->last = newNode;
+    }
+    // if we HAVE a comparison function, we use that for insertion
+    else
+    {
+        // start from the first node and go back
+        QNode **n = &queue->first;
+        // keeps goint unil we find a spot to put our new item in
+        // while: n is not null, and we are still greater than selected element
+        while(*n != NULL && queue->cmp((*n)->data, data) > 0)
+            // sets our n to the next node based upon 
+            n = &(*n)->next;
+
+        // we've escaped our loop, we found where we should input newNode
+        // if *n is NULL, we've hit the end of our queue
+        if(*n == NULL)
+            queue->last = newNode;
+        else
+            
+    }
+
+    // adds one to the size of the queue (we just added one in)
+    queue->size += 1;
 }
 
 
