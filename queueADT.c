7ee42b76b0ece3f7287cf07907d2090c1a399954
Kevin Joseph Becker <kjb2503@cs.rit.edu>
2018-03-13 21:32:35 -0400

    Removed an excess include which was not used (oops).

--- queueADT.c
+++ queueADT.c
@@ -7,7 +7,6 @@
 ///
 // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
-#include <stdio.h> //
 #include <stdlib.h> // malloc, free
 #include <stdbool.h> // boolean data members
 
